5c355df0406cce7a48f16ee157a441a085116bfb
AntonNguyen <afnguyen85@gmail.com>
2013-05-11 10:57:31 -0400

    Fixed the tests, so that they actually do work

--- test/winston-posix-syslog-test.js
+++ test/winston-posix-syslog-test.js
@@ -1,13 +1,42 @@
-var vows = require('vows')
+var path = require('path')
+  , vows = require('vows')
   , assert = require('assert')
   , winston = require('winston')
   , helpers = require('winston/test/helpers')
-  , transport = require('winston/test/transports/transport')
   , PosixSyslog = require('../lib/winston-posix-syslog').PosixSyslog
+  , npmTransport = new (PosixSyslog)()
+  , posixSyslogTransport = new (PosixSyslog)({ levels: winston.config.syslog.levels })
   ;
 
+var assertPosixSyslog = function(transport) {
+  assert.instanceOf(transport, PosixSyslog);
+  assert.isFunction(transport.log);
+};
+
 vows.describe('winston-posix-syslog').addBatch({
-  "An instance of the PosixSyslog Transport": transport(PosixSyslog, {
-    identity: 'test'
-  })
+  "Instantiation of the PosixSyslog Transport": {
+    "With no options should not fail": function() {
+      winston.add(PosixSyslog);
+    }
+  },
+  "An instance of the PosixSyslog Transport": {
+    "with npm levels": {
+      "should have the proper methods defined": function () {
+        assertPosixSyslog(npmTransport);
+      },
+      "the log() method": helpers.testNpmLevels(npmTransport, "should respond with true", function (ign, err, logged) {
+        assert.isNull(err);
+        assert.isTrue(logged);
+      })
+    },
+    "with syslog levels": {
+      "should have the proper methods defined": function () {
+        assertPosixSyslog(posixSyslogTransport);
+      },
+      "the log() method": helpers.testSyslogLevels(posixSyslogTransport, "should respond with true", function (ign, err, logged) {
+        assert.isNull(err);
+        assert.isTrue(logged);
+      })
+    }
+  }
 }).export(module);
